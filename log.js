1dfdc97cd269345262991beb14fd31ac7cfc7c5c
WMBYBE <williambbrouillette@gmail.com>
2024-01-17 11:20:44 -0600

    fixed js even more

--- log.js
+++ log.js
@@ -7,5 +7,5 @@ test += 100;
 
 let answer = prompt("What is your favorite day of the week?");
 
-console.log("variable labeled test started with a value of 25 and is now at" + test)
-console.log("The variable labeled answer was prompted and return" + answer);
+console.log("variable labeled test started with a value of 25 and is now at" + " " + test)
+console.log("The variable labeled answer was prompted and return" + " " + answer);
