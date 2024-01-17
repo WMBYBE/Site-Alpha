5e8dcf5251b3a5587ef2d11aae4d44d8c3aa73b8
WMBYBE <williambbrouillette@gmail.com>
2024-01-17 11:19:43 -0600

    fixed js file

--- log.js
+++ log.js
@@ -1,16 +1,5 @@
 /* William Brouillette
-1/17/2024
-    1. Open your Portfolio repository
-    2. Add new page to your site for testing
-    3. Write a js file with your name and the current date as a comment at the top
-    4. Add a new variable to the file and set it to an integer value
-    5. Preform a mathematical operation on the integer value, and store it in that variable
-    6. Create a variable and prompt the user for a value to put into the variable (maybe ask about their favorite things?)
-    7. Use the console.log() function to print both variables to the console with the message telling me what the variable name is,
-    and what it's value is
-    8. Push your changes to github
-    9. Screenshot the console with both of your variables printed to it
-    */
+1/17/2024*/
 
 let test = 25
 
@@ -18,4 +7,5 @@ test += 100;
 
 let answer = prompt("What is your favorite day of the week?");
 
-console.log("variable labeled test started with a value of 25 and is now at" + test, /n "The variable labeled answer was prompted and return" + answer);
+console.log("variable labeled test started with a value of 25 and is now at" + test)
+console.log("The variable labeled answer was prompted and return" + answer);
