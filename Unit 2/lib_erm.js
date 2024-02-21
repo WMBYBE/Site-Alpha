d1c6fae42dc919e07794740d904467ce6493980a
WMBYBE <williambbrouillette@gmail.com>
2024-02-21 11:27:13 -0600

    unit 2 begins

---
+++ Unit 2/lib_erm.js
@@ -0,0 +1,7 @@
+class fruit {
+    constructor(name, flavor){
+        this.name=name
+        this.flavor=flavor
+        console.log(`the ${this.name} is very ${this.flavor}`)
+    }
+}
---
+++ Unit 2/objects.js
@@ -0,0 +1,3 @@
+const melon = new fruit("Watermelon", "spicy");
+const apple = new fruit("Apple","sour")
+const lemon = new fruit("Lemon", "earthy")
